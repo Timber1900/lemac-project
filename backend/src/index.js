bdda06fa7c2a2eb2d89a70d6da78682f2b5c8324
Timber1900 <Timber.gamer@gmail.com>
2022-09-16 16:44:20 +0000

    changed cors options

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,7 +11,7 @@ const api = require('./api');
 
 //enables CORS in dev server
 var corsOptions = {
-  origin: [process.env.URL, "http://dev4.dem.tecnico.ulisboa.pt"],
+  origin: [process.env.URL, "localhost:5000"],
 };
 
 console.log(corsOptions.origin)
